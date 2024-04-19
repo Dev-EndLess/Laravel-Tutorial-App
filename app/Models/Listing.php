7d62168e7f3ceb5bae0cd413413b45bd4610b7a7
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2024-04-19 14:33:14 +0200

    search filter

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -12,7 +12,14 @@ class Listing extends Model
     public function scopeFilter($query, array $filters ) {
         if($filters["tag"] ?? false ) {
             $query->where("tags","like","%". request('tag') ."%");
-    }
+        }
+
+        if($filters["search"] ?? false ) {
+          $query->where("title","like","%". request('search') ."%")
+          ->orWhere("company","like","%". request("search") ."%")
+          ->orWhere("tags","like","%". request("search") . "%")
+          ->orWhere("location","like","%". request("search") . "%");
+      }
 
 }
 }
